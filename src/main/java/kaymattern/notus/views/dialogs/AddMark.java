3b68d17f47430caaa56fd52a78e4c2483632e82c
xooback <kay.mattern@gibmit.ch>
2018-03-21 17:33:23 +0100

    Add dialog controllers

--- src/main/java/kaymattern/notus/views/dialogs/AddMark.java
+++ src/main/java/kaymattern/notus/views/dialogs/AddMark.java
@@ -1,4 +1,25 @@
 package kaymattern.notus.views.dialogs;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+
+import java.awt.TextField;
+
 public class AddMark {
+
+    @FXML private TextField nameTextField;
+    @FXML private DatePicker datePicker;
+    @FXML private TextField markTextField;
+    @FXML private TextField weightTextField;
+
+    @FXML
+    private void cancel() {
+
+    }
+
+    @FXML
+    private void add() {
+
+    }
+
 }
--- src/main/java/kaymattern/notus/views/dialogs/AddSubject.java
+++ src/main/java/kaymattern/notus/views/dialogs/AddSubject.java
@@ -1,4 +1,21 @@
 package kaymattern.notus.views.dialogs;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class AddSubject {
+
+    @FXML private TextField nameTextField;
+
+    @FXML
+    private void cancel() {
+
+    }
+
+    @FXML
+    private void add() {
+
+    }
+
 }
